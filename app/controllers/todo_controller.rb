c68fdc8f69e25e31880af8a1bdc94b4e9510f3b6
derek <derekb5789@gmail.com>
2018-07-11 15:05:31 +0000

    greetings

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,8 @@
 class TodoController < ApplicationController
     def index
+        def show
+        @todo_description = "make eggs"
+        @todo_pomodoro_estimate = "7"
+        end
     end
 end
