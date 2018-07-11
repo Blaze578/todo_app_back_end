44788fd2a3bc852d886523675ac11007869070bc
derek <derekb5789@gmail.com>
2018-07-11 17:44:29 +0000

    greetings

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,5 +5,13 @@ class TodoController < ApplicationController
     def show
         @todo_description = "make eggs"
         @todo_pomodoro_estimate = "7"
+        todo_id = params [id]
+        if todo_id == '1'
+            @todo_description = "im changed"
+            @todo_pomodoro_estimate = '100'
+            elsif todo_id == '5'
+            @todo_description = 'hello' 
+            @todo_pomodoro_estimate = '10'
+        end
     end
 end
